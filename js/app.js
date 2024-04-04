61037f64665eb3574eb0d146070aa011395ba2c2
zeurgoAlice <alicezcruz2018@gmail.com>
2024-04-04 08:41:57 -0300

    aaa

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const traduzir = async(texto) => {
     const textTraducao = texto
     
     //pegando a api
-    const url = `https://api.mymemory.translated.net/get?q=${textTraducao}&langpair=pt-br|en|spa`
+    const url = `https://api.mymemory.translated.net/get?q=${textTraducao}&langpair=pt-br|en`
 
     // requisitando a api
     const response = await fetch(url)
@@ -24,7 +24,7 @@ const traduzir = async(texto) => {
 }
 
 // função para mostrar o texto traduzido no input PARA
-export const montarForm = async() => {
+const montarForm = async() => {
 
     // envia o texto a ser traduzido e recebe a tradução
     const textTraduzido = await traduzir(inputFrom.value)
@@ -36,3 +36,4 @@ export const montarForm = async() => {
 
 // adicionando um ação para o input, no caso dessa "change" será quando o input mudar
 inputFrom.addEventListener('change', montarForm)
+
--- js/darkmode.js
+++ js/darkmode.js
@@ -1,73 +1,55 @@
 'use strict'
 
- import { montarForm } from "./app.js";
+// o evento "DOMContentLoaded" garante que o HTML seja totalmente carregado antes do código ser executado
+document.addEventListener("DOMContentLoaded", async() => {
 
-// recebe o elemento HTML
-const darkMode = document.getElementById('dark-mode')
+    // recebe o elemento HTML
+    const darkMode = document.getElementById('dark-mode')
 
-// pega o input do texto a ser traduzido
-const inputFrom = document.getElementById('input-de')
+    // pega o input do texto a ser traduzido
+    const inputFrom = document.getElementById('input-de')
 
-//pega o input do texto traduzido
-const inputTo = document.getElementById('input-para')
+    //pega o input do texto traduzido
+    const inputTo = document.getElementById('input-para')
 
-// recebe o body do html
-const body = document.body
+    // recebe o body do html
+    const body = document.body
 
-// inicializa a variável
-let modoEscuro = false 
+    // inicializa a variável
+    let modoEscuro = false
 
-// função para trocar a cor do fundo
-const trocarBackground = async() => {
+    // adiciona evento de click no elemento
+    darkMode.addEventListener('click', async() => {
 
-    // muda o valor da variável de false p true
-    modoEscuro = !modoEscuro
-    
-    if(modoEscuro){
-        
-        // quando o modo escuro tiver ativado, define a imagem do sol
-        darkMode.src = '../imgs/sol.svg'
-        
-        // muda a cor do fundo para preto
-        body.style.background = 'black'
-        
-        // muda a cor do texto dos inputs para branco
-        inputFrom.style.color = 'white'
-        inputTo.style.color = 'white'
-        
-    } else {
-        
-        //  quando o modo escuro tiver desativado, define a imagem da lua
-        darkMode.src = '../imgs/lua.svg'
-        
-        // reverte ao padrão
-        body.style.background = ''
-        
-        // reverte ao padrão
-        inputFrom.style.color = 'white'
-        inputTo.style.color = 'white'
-        
-    }
+        // muda o valor da variável de false p true
+        modoEscuro = !modoEscuro
 
-}
+        // se(modoEscuro ativado)
+        if(modoEscuro){
 
-// criando a feature alice, que quando o input receber a palavra muda a cor do background
-const alice = async() => {
-        
-    if (inputFrom.value == 'alice'){
+            // quando o modo escuro tiver ativado, define a imagem do sol
+            darkMode.src = '../imgs/sol.svg'
 
-        trocarBackground()        
-        
-    } else {
+            // muda a cor do fundo para preto
+            body.style.background = 'black'
 
-        montarForm()
+            // muda a cor do texto dos inputs para branco
+            inputFrom.style.color = 'white'
+            inputTo.style.color = 'white'
 
-    }
+        } else {
 
-}
+            //  quando o modo escuro tiver desativado, define a imagem da lua
+            darkMode.src = '../imgs/lua.svg'
 
-// adiciona evento de click no elemento
-darkMode.addEventListener('click', trocarBackground)
+            // reverte ao padrão
+            body.style.background = ''
 
- // adicionando um ação para o input, no caso dessa "change" será quando o input mudar
-inputFrom.addEventListener('change', alice)
+            // reverte ao padrão
+            inputFrom.style.color = 'white'
+            inputTo.style.color = 'white'
+
+        }
+
+    })
+})
